4ea9b0c61178a13c9f80765085dab4d98e791b12
saklain31 <saklain78@gmail.com>
2017-03-02 21:56:00 +0600

    welcome activity

---
+++ app/src/main/java/com/example/saklain/child/WelcomeActivity.java
@@ -0,0 +1,76 @@
+package com.example.saklain.child;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class WelcomeActivity extends Activity implements AnimationListener {
+
+    TextView txtMessage;
+
+    // Animation
+    Animation animFadein;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        txtMessage = (TextView) findViewById(R.id.welcomeMsg);
+
+
+        // load the animation
+        animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_in);
+
+        // set animation listener
+        animFadein.setAnimationListener(this);
+
+        Runnable greet = new Runnable() {
+            @Override
+            public void run() {
+                txtMessage.setVisibility(View.VISIBLE);
+
+                // start the animation
+                txtMessage.startAnimation(animFadein);
+            }
+        };
+        Thread welcome = new Thread(greet);
+        welcome.start();
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // Take any action after completing the animation
+
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Intent in3 = new Intent(WelcomeActivity.this,MainActivity.class);
+        startActivity(in3);
+        finish();
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+}
